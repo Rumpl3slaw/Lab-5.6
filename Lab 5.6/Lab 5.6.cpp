f190c49c7e05e76aed83523b6b0c746755f2659a
Rumpl3slaw <wowpanda13@ukr.net>
2020-11-08 13:06:26 +0200

    Lab 5.6

---
+++ Lab 5.6/Lab 5.6.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double h(const double a, const double b);
+int main()
+{
+	double s, t;
+
+	cout << "s=";cin >> s;
+	cout << "t=";cin >> t;
+
+	double R = (pow(h(s, t), 4) + h(1, s * s + t * t)) / (1 + pow(h(s * t, 1), 2));
+	cout << "R=" << R << endl;
+
+	return 0;
+}
+double h(const double a, const double b)
+{
+	return (a / (b * b + 1) + 1. / (a * a + b * b));
+}
